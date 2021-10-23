f3b7fe7e5ab56b993d0cf3de4098e281206a0d7e
SergeyChumakov <webk0d@ya.ru>
2021-10-23 17:10:19 +0300

    done

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var labelRed: UILabel!
+    @IBOutlet weak var labelGreen: UILabel!
+    @IBOutlet weak var labelBlue: UILabel!
+    
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewColor.layer.cornerRadius = 10
+        
+        sliderRed.value = 0
+        sliderGreen.value = 0
+        sliderBlue.value = 0
+        
+        sliderRed.minimumTrackTintColor = .red
+        sliderGreen.minimumTrackTintColor = .green
+        sliderBlue.minimumTrackTintColor = .blue
     }
-
-
+    
+    
+    @IBAction func sliserRedAction() {
+        labelRed.text = String(format: "%.2f", sliderRed.value)
+        viewColor.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
+    
+    @IBAction func sliserGreenAction() {
+        labelGreen.text = String(format: "%.2f", sliderGreen.value)
+        viewColor.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
+    
+    @IBAction func sliserBlueAction() {
+        labelBlue.text = String(format: "%.2f", sliderBlue.value)
+        viewColor.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+    }
+    
 }
 
